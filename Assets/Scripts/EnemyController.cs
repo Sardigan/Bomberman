d66aa0385851d0ff0c5bc4baba59a1a26a4ed1c5
Sardigan <gal-kh@mail.ru>
2014-11-14 20:47:02 +0300

    TestEnemyController

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,22 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
 
-    public Controller enemy;  
-	private float move_x;
-	// Use this for initialization
-	void Start () {
+		public Controller enemy;
+		private float move_x = 1;
+		private float move_z = 0;
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
-	void Star () {
-		move_x = 2;
-		
-	}
+		}
 
-	// Update is called once per frame
-	void Update () {
-		this.Star ();
-		enemy.Controlling(1, move_x, 2);
-	}
+		void OnCollisionEnter (Collision col)
+		{
+				if (col.gameObject.name == "wall1") {
+						move_x = 1;
+						move_z = 0;
+				}
+				if (col.gameObject.name == "wall2") {
+						move_x = 0;
+						move_z = 1;
+				}
+				if (col.gameObject.name == "wall3") {
+						move_x = -1;
+						move_z = 0;
+				}
+				if (col.gameObject.name == "wall4") {
+						move_x = 0;
+						move_z = -1;
+				}
+		
+		}	
+	
+		// Update is called once per frame
+		void Update ()
+		{		
+				enemy.Controlling (1, move_x, move_z);
+		}
 }
