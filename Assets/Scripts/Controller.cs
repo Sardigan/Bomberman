ea30b5f2d371110a31f0f5fcba1cadacdd31fd52
https://Galimoff@bitbucket.org/Galimoff/fp.git <Gal-kh@mail.ru>
2015-05-07 00:24:14 +0300

    Test

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,8 +6,7 @@ public class Controller: MonoBehaviour
 			
 	public void Controlling (float speed, float move_x, float move_y)
 	{
-		Rigidbody2D myRigidBody = GetComponent<Rigidbody2D>();
-		//GetComponent<Rigidbody2D>().velocity = new Vector2 (move_x * speed, move_y * speed);	
+		Rigidbody2D myRigidBody = GetComponent<Rigidbody2D>();			
 		myRigidBody.velocity = new Vector2 (move_x * speed, move_y * speed);
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,24 +10,69 @@ public class PlayerController : MonoBehaviour
 		private Animator anim;
 		private bool isFacingLeft = true;
 		public Transform activator;
+		public int move_x = 0;
+		public int move_y = 0;
+		public bool b = false;
+		public bool OnClick = false;
 
 		void Start ()
 		{
-				anim = GetComponent<Animator> ();	
+				anim = GetComponent<Animator> ();
+				
 		}
 
-		void FixedUpdate ()
-		{	
-				player.Controlling (speed, Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		public void Up ()
+		{
+				move_y = 1;
+				OnClick = true;
 		}
+
+		public void Down ()
+		{
+				move_y = -1;
+				OnClick = true;
+		}
+
+		public void Right ()
+		{
+				move_x = 1;
+				OnClick = true;
+		}
+
+		public void Left ()
+		{
+				move_x = -1;
+				OnClick = true;
+
+
+		}
+
+		public void B ()
+		{
+				b = true;
+		}
+		
+		void FixedUpdate ()
+		{
+				if (OnClick == true) {
+						player.Controlling (speed, move_x, move_y);
+						OnClick = false;
+				}
+				player.Controlling (speed, (int)Input.GetAxis ("Horizontal"), (int)Input.GetAxis ("Vertical"));
+		}
+
 		// Update is called once per frame
-		void Update ()
+		public void Update ()
 		{				
 				int x = Mathf.RoundToInt (transform.position.x);
 				int y = Mathf.RoundToInt (transform.position.y);
 
-				anim.SetInteger ("SpeedV", (int)Input.GetAxis ("Vertical"));
-				anim.SetInteger ("SpeedH", (int)Input.GetAxis ("Horizontal"));
+				//anim.SetInteger ("SpeedV", (int)Input.GetAxis ("Vertical"));
+				//anim.SetInteger ("SpeedH", (int)Input.GetAxis ("Horizontal"));
+
+				//anim.SetInteger ("SpeedH", move_x);
+				//anim.SetInteger ("SpeedV", move_y);
+
 		
 				if (x % 2 != 0) {
 						x = x - 1;
@@ -36,13 +81,15 @@ public class PlayerController : MonoBehaviour
 				if (y % 2 != 0) {
 						y = y - 1;
 				}
+
 			
-				if (Input.GetKeyDown ("space") && exists == false) {            
+				if ((Input.GetKeyDown ("space") || b == true) && exists == false) {            
 						Instantiate (bomb, new Vector3 (x, y, transform.position.z), 
 				             Quaternion.identity);	
 						Instantiate (activator, new Vector3 (x, y, transform.position.z), 
 			             Quaternion.identity);
-						exists = true;						
+						exists = true;	
+						b = false;
 				}
 				if (GameObject.Find ("Activator(Clone)") == null) {
 						exists = false;
